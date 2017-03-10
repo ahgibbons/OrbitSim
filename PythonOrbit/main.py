2fb8bbd0acbda087b4d4a73b352daab2adbdbf6b
Andrew <Andrew@li-ys-MacBook-Pro.local>
2017-03-11 00:33:46 +0900

    Finished Python Orbit program

--- PythonOrbit/main.py
+++ PythonOrbit/main.py
@@ -9,6 +9,7 @@ OrbitSim
 
 import numpy as np
 import pandas as pd
+import sys
 
 G = 1
 
@@ -69,32 +70,51 @@ class TestMass:
         return f
                 
 
-def main():
-    finalTime = 10
-    totalSteps = 100
-    dt = finalTime / totalSteps
+def main(finalTime,totalSteps):
+    dt = float(finalTime) / totalSteps
     
-    m1 = TestMass(4,0,0,2,dt,1)
+    m1 = TestMass(3,0,0,0.5,dt,1)
     m0 = TestMass(0,0,0,0,dt,1)
     
     m1.set_otherMasses([m0])
     
     allMasses = [m0,m1]
     
-    xs = np.array([0]*totalSteps)
-    ys = np.array([0]*totalSteps)
+    xs = np.array([0.0]*totalSteps)
+    ys = np.array([0.0]*totalSteps)
     
-    t = 0
-    while t<finalTime:
+    
+    for n in np.arange(totalSteps):
         for m in allMasses:
             m.stepStart()
         for m in allMasses:
             m.stepEnd()
-        xs.append(m1.r[0])
-        ys.append(m1.r[1])
-        t += dt
+        xs[n] = m1.r[0]
+        ys[n] = m1.r[1]
         
     ts = np.linspace(0,finalTime,totalSteps)
     
-    return(xs,ys,ts)
+    return np.array([xs,ys,ts]).T
     
+if __name__ == "__main__":
+    """
+    args: [Final time] [Total steps]
+    """
+    
+    argv = sys.argv
+
+    if len(argv)!=3:
+        raise Exception("Incorrect number of arguments!")
+
+    
+    final_time = float(argv[1])
+    total_steps = int(argv[2])
+
+    res = main(final_time,total_steps)
+    print("Simulation Complete")
+
+    df = pd.DataFrame(res,columns=['x','y','t'])
+    df.to_csv("orbit_out.csv",sep=',')
+    print("Data saved to orbit_out.csv")
+    
+
