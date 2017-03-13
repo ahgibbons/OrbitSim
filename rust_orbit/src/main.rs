020ea849814ebecb0880a8ed9017cb52f0f2a0b7
Andrew.Windows <andrew.h.gibbons@gmail.com>
2017-03-13 11:59:42 +0900

    started rust orbit program

--- rust_orbit/src/main.rs
+++ rust_orbit/src/main.rs
@@ -1,4 +1,7 @@
 use std::f64;
+use std::vec;
+
+const G_CONST: f64 = 1.0;
 
 struct Mass {
     x: f64,
@@ -10,19 +13,43 @@ struct Mass {
     yn: f64,
 }
 
+fn grav(mass1: Mass, mass2: Mass) -> (f64,f64) {
+    // GM1M2 / (r^3/2) * (x)
+    let A = G_CONST * mass1.m * mass2.m;
+    let dx = mass2.x - mass1.x;
+    let dy = mass2.y - mass1.y;
+    let r_2 = dx*dx + dy*dy;
+    let r_32 = f64::powi(f64::sqrt(r_2),3);
+
+    (A*dx/r_32, A*dy/r_32)
+}
+
+fn init_mass(x: f64, y: f64, vx: f64, vy: f64, dt: f64) -> Mass {
+    let x1 = x + vx*dt;
+    let y1 = y + vy*dt;
+    Mass {x: x1, y: y1, m: 1.0, x0: x, y0: y, xn: 0.0, yn: 0.0}
+}
+
+fn verlet_step(r0: f64, r1: f64, a: f64, dt: f64) -> f64 {
+    2.0*r1 - r0 + dt*dt*a
+}
+
 fn main() {
-    
     let final_time : f64 = 10.0;
     let total_steps : i32 = 100;
+    let dt : f64 = final_time / (total_steps as f64);
+    let m1 = init_mass(10.0,0.0,0.0,0.3,dt);
+    let m2 = init_mass(0.0,0.0,0.0,0.0,dt);
 
-    let m1 = Mass {x:10, y:0.01, m:1, x0:10, y0:0,
-                   xn:0,yn:0};
-
-    let m2 = Mass {x:0, y:0.0, m:1, x0:0, y0:0,
-                   xn:0,yn:0};
+    let xs: Vec<f64> = vec![0.0;total_steps as usize];
+    let ys: Vec<f64> = vec![0.0;total_steps as usize];
+    let ts: Vec<f64> = vec![0.0;total_steps as usize];
     
-   
-   println!("Hello, world!");
+    let (fx,fy) = grav(m1,m2);
+    
+    println!("dt is: {}", dt);
+    println!("(fx,fy) is: ({}, {})", fx, fy);
+    println!("Hello, world!");
 }
 
 
