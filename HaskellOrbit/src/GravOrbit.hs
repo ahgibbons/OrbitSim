82e84452bd12f57eb6ae811b8a36f7ecdfe2f50b
Andrew-Ubuntu <andrew.h.gibbons@gmail.com>
2017-03-18 18:04:41 +0900

    Added to Haskell Orbit program

--- HaskellOrbit/src/GravOrbit.hs
+++ HaskellOrbit/src/GravOrbit.hs
@@ -2,15 +2,39 @@ module GravOrbit where
 
 import System.Environment (getArgs)
 
-g :: Double
-m :: Double
-g = 1
-m = 1
+gConst :: Double
+gConst = 1
 
 data Pos = Pos Double Double deriving (Show,Eq)
 data Acc = Acc Double Double deriving (Show,Eq)
+data Force = Force Double Double deriving (Show,Eq)
 type Mass = Double
+data DynMass = DynMass { mmass :: Mass, mpos :: Pos
+                       , mprevpos :: Pos, mID :: Int} deriving (Show,Eq)
 
+fadd :: Force -> Force -> Force
+fadd (Force fx1 fy1) (Force fx2 fy2) = Force (fx1+fx2) (fy1+fy2)
+             
+mass0 = DynMass {mmass=1, mpos=Pos 0 0, mprevpos=Pos 0 0, mID=0}
+mass1 = DynMass {mmass=1, mpos=Pos 9 0, mprevpos=Pos 8.9 0, mID=1}
+
+massEq :: DynMass -> DynMass -> Bool
+massEq m1 m2 = mID m1 == mID m2
+        
+allMasses :: [DynMass]
+allMasses = [mass0,mass1]
+
+f2m :: Force -> Mass -> Acc
+f2m (Force fx fy) m' = Acc (fx/m') (fy/m')
+
+genForces :: DynMass -> [DynMass] -> [Force]
+genForces mass masses = map (mGrav gConst mass) (filter (not . massEq mass) masses)
+            
+mGrav :: Double -> DynMass -> DynMass -> Force
+mGrav g' m1 m2 = gravForce g' (mmass m1) (mmass m2) (mpos m1) (mpos m2)
+
+gravStep :: [DynMass] -> 
+                 
 perpV :: Double -> Mass -> Double -> Double
 perpV g' m' r = sqrt(g'*m'/r)
 
@@ -25,14 +49,9 @@ verletStep dt (Acc ax ay) (Pos x0 y0, Pos x1 y1) = (Pos x1 y1, Pos x' y')
         verletStep1D a r0 r1 = 2*r1 - r0 + (dt*dt*a)
  
 gravForce :: Double -> Mass -> Mass
-          -> Pos -> Pos -> Acc
-gravForce g' m1 m2 (Pos x2 y2) (Pos x1 y1) = Acc (f*dx) (f*dy)
+          -> Pos -> Pos -> Force
+gravForce g' m1 m2 (Pos x2 y2) (Pos x1 y1) = Force (f*dx) (f*dy)
   where
-    f  = g'*m1*m2 / ((x2-x1)**2 + (y2-y1)**2)**(3.0/2)
+    f  = g'*m1*m2 / (dx**2 + dy**2)**(3.0/2)
     dx = x2-x1
     dy = y2-y1
-
-orbitStep g' m1 m2 
-
-runOrbit :: Double -> Mass -> Mass -> Pos -> Pos -> [Pos]
-runOrbit g' m1 m2 p1 p2 = iterate (
--- PythonOrbit/pyplotOrbit.py
+++ PythonOrbit/pyplotOrbit.py
@@ -7,32 +7,33 @@ import sys
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def plotOrbit(infile,outfile,nplots):
+def plotOrbit(infile,outfile):
         df = pd.read_csv(infile,sep=' ',header=0)
+
+        nplots = len(df.columns)
 
 
         fig = plt.figure()
-        ax = fig.add_subplot(111)
+        ax = fig.add_subplot(111,aspect='equal')
 
-        for n in range(nplots):
-                xkey = 'x'+str(n)
-                ykey = 'y'+str(n)
+        for n in range(1,nplots,2):
+                xkey = df.columns[n]
+                ykey = df.columns[n+1]
                 xs = df[xkey].get_values()
                 ys = df[ykey].get_values()
                 ax.plot(xs,ys)
 
         plt.savefig(outfile)
 
-        print("Image made!")
+        print("Image made! Plotted trajectory of {:d} mass(es).".format((nplots-1)//2))
 
         return 1
 
 def main(argv):
         infile = argv[1]
         outfile = argv[2]
-        nplot = int(argv[3])
 
-        plotOrbit(infile,outfile,nplot)
+        plotOrbit(infile,outfile)
 
 if __name__=="__main__":
         argv = sys.argv
